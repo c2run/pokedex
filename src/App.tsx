fe8df9e0058d75e2252d0092d348edb405c6d58c
c2run <jose.cerda66@gmail.com>
2023-03-23 09:49:22 -0300

    se agregan rutas de react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      <header className="App-header">
-        <h1>Pokedex</h1>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/pokemons/:name" element={<Pokemon />} />
+      <Route path="/pokemon" element={<Pokemons />} />
+      <Route path="/items" element={<Items />} />
+      <Route path="/" element={<Pokemons />} />
+    </Routes>
+
+    
   );
 }
 
