f29287f35b1b9c551d8e7188f130628bafcc5dfe
c2run <jose.cerda66@gmail.com>
2023-03-23 10:54:34 -0300

    header query setquery pasar var props

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,20 @@
 
 import styles from './header.module.css';
 
-const Header = () => {
+type HeaderProps = {
+    query: string;
+    setQuery: (query: string) => void;
+};
+
+const Header = ({query, setQuery}: HeaderProps) => {
     return (
       <header className={styles.header}>
         <input className={styles.input}
          placeholder='Buscar pokemon' 
-         type="text" />
+         type="text"
+         value={query}
+         onChange={(event) => setQuery(event.target.value)} 
+         />
       </header>
     );
   };
--- src/pages/Pokemons.tsx
+++ src/pages/Pokemons.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import Header from '../components/Header';
+
 const Pokemons = () => {
+    const [query, setQuery] = useState("Bulbasaur");
   return (
    <>
-   <Header />
-
+   <Header query={query} setQuery={setQuery} />
    <main>
         <h1>Pokemons</h1>
    </main>
