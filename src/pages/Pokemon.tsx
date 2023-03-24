cb3c3b32b3993f5b4c6a9cf6f1119f6452a6b9b1
c2run <jose.cerda66@gmail.com>
2023-03-24 11:50:53 -0300

    estilo css página detalle pokemon

--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
+import PokeballImg from "../assets/pokeball.png";
+import BulbasaurImg from "../assets/bulbasaur.gif";
+import Footer from "../components/Footer";
+import styles from "./pokemon.module.css";
+
 const Pokemon = () => {
-    const {name} = useParams();
+  const {name} = useParams();
+  const navigate = useNavigate();
   return (
-    <div>{name}</div>
-    
+    <>
+    <button className={styles.pokeballButton} onClick={() => navigate(-1)}>
+      <img className={styles.pokeballImg} src={PokeballImg} alt="" /> Volver
+    </button>
+    <div className={styles.pokeballButton}>
+      <main className={styles.pokemonInfo}>
+        <div className={styles.pokemonTitle}>{name?.toUpperCase()}</div>
+        <div>Nr. 001</div>
+        <div>
+          <img className={styles.pokemonInfoImg} src={BulbasaurImg} alt="bulbasaur" />
+        </div>
+        <div>HP: 000</div>
+        <div>Ataque: 000</div>
+        <div>Defensa: 000</div>
+      </main>
+    </div>
+    <Footer />
+    </>
   )
 }
 
