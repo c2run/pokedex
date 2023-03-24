f6e5adde0b582e7858c1ec34e004babcbc0376d1
c2run <jose.cerda66@gmail.com>
2023-03-24 14:26:25 -0300

    pantalla de carga para visualización de objeto pkm

--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -7,21 +7,27 @@ import { fetchPokemon } from "../api/fetchPokemon";
 import { useEffect } from "react";
 import {useState} from "react";
 import { PokemonDetails } from "../types/types";
+import LoadingScreen from "../components/LoadingScreen";
+import { waitFor } from "../utils/utils";
 
 const Pokemon = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [pokemon, setPokemon] = useState<PokemonDetails>();
   const {name} = useParams();
   const navigate = useNavigate();
 
     useEffect(() => {
        async function getPokemon(){
+        setIsLoading(true);
+        await waitFor(500);
         const fetchedPokemon = await fetchPokemon(name as string);
         setPokemon(fetchedPokemon);
+        setIsLoading(false);
       }
       getPokemon();
     }, [name]);
     
-
+    if(isLoading || !pokemon) return <LoadingScreen />;
 
   return (
     <>
@@ -30,7 +36,7 @@ const Pokemon = () => {
     </button>
     <div className={styles.pokeballButton}>
       <main className={styles.pokemonInfo}>
-        <div className={styles.pokemonTitle}>{name?.toUpperCase()}</div>
+        <div className={styles.pokemonTitle}>{pokemon?.name?.toUpperCase()}</div>
         <div>{pokemon?.id}</div>
         <div>
           <img className={styles.pokemonInfoImg} src={pokemon?.imgSrc} alt={pokemon?.name} />
